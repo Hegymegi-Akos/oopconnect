cc9dd1b7f57568e89669b50a465306115635822d
Ákos <hegymegi-kissa@kkszki.hu>
2025-10-03 10:24:28 +0200

    sikeres feltöltés

--- Test/Controllers/Connect.cs
+++ Test/Controllers/Connect.cs
@@ -1,6 +1,28 @@
-﻿namespace Test.Controllers
+﻿using MySql.Data.MySqlClient;
+
+namespace Test.Controllers
 {
     public class Connect
     {
+        public MySqlConnection ? Connection;
+
+        private string _host;
+        private string _db;
+        private string _user;
+        private string _password;
+
+        private string ConnectionString;
+
+        public Connect()
+        {
+            _host = "localhost";
+            _db = "testdb";
+            _user = "root";
+            _password = "password";
+            ConnectionString = $"SERVER={_host};DATABASE={_db};UID={_user};PASSWRD={_password};";
+
+
+        }
     }
 }
+
